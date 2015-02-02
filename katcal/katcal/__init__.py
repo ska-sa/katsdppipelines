9435219b206a8577f0fe2f0fefa4a199e9dfd887
LauraRichter <laura@ska.ac.za>
2015-02-02 11:32:01 +0200

    Added timestamps and some formatting to logging messages

--- katcal/katcal/__init__.py
+++ katcal/katcal/__init__.py
@@ -6,10 +6,8 @@ Calibration pipeline package for MeerKAT.
 """
 
 import logging
-logging.basicConfig(filename='pipeline.log')
+logging.basicConfig(filename='pipeline.log',
+                    format='%(asctime)s %(name)-24s %(levelname)-8s %(message)s',
+                    datefmt='%d-%m-%y %H:%M',)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-#LOG_FILENAME='/var/log/celery_workflowmgr/celery_workflowmgr.log'
-#logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
