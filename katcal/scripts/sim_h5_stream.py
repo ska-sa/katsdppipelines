07c2d66eb26a3df694e6bd790d14743d21c2c2b2
Thomas Bennett <thomas@ska.ac.za>
2015-02-06 00:42:33 +0200

    Beutify scripts. Add shebang. Call get_args() function. Turn comments into print statements.

--- katcal/scripts/sim_h5_stream.py
+++ katcal/scripts/sim_h5_stream.py
@@ -1,34 +1,33 @@
-
-import optparse
-import sys
-
-from katcal.simulator import SimData
-
-from katsdptelstate.telescope_state import TelescopeState
-
+#!/usr/bin/env python
 # ----------------------------------------------------------
 # H5 file to use for simulation
-#   simulation of data and Teselcope Model (TM)
+#   simulation of data and Teselcope State (TS)
 
-parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description='Simulate SPEAD data stream from H5 file.')
-parser.add_option("--ts_db", default=1, type="int", help="Telescope state database number. default: 1")
-parser.add_option("--ts_ip", default="127.0.0.1", help="Telescope state ip address. default: 127.0.0.1")
-(options, args) = parser.parse_args()
 
-if len(args) < 1 or not args[0].endswith(".h5"):
-    print "Please provide an H% filename as argument"
-    sys.exit(1)
-        
-file_name = args[0]
-simdata = SimData(file_name)
+import optparse
 
-# use TM set up by sim_h5_ts.py and run_cal.py scripts
+from katcal.simulator import SimData
+from katsdptelstate.telescope_state import TelescopeState
+
+def get_args():
+    parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description='Simulate SPEAD data stream from H5 file.')
+    parser.add_option("--ts-db", default=1, type="int", help="Telescope state database number. default: 1")
+    parser.add_option("--ts-ip", default="127.0.0.1", help="Telescope state ip address. default: 127.0.0.1")
+    (options, args) = parser.parse_args()
+    if len(args) < 1 or not args[0].endswith(".h5"):
+        parser.error("Please provide a .h5 filename as argument")
+    return args[0], options
+
+filename, options = get_args()
+simdata = SimData(filename)
+
+print "Use TS set up by sim_h5_ts.py and run_cal.py scripts."
 ts = TelescopeState(host=options.ts_ip,db=options.ts_db)
-    
-# select data to transmit         
+
+print "Selecting data to transmit. Slice using ts values."
 simdata.select(channels=slice(ts.bchan,ts.echan))
 
-# transmit data
+print "TX: start."
 simdata.h5toSPEAD(ts,8890)
-print 'TX: ended.'
+print "TX: ended."
 
--- katcal/scripts/sim_h5_ts.py
+++ katcal/scripts/sim_h5_ts.py
@@ -10,20 +10,22 @@ from katcal.simulator import SimData
 from katsdptelstate.telescope_state import TelescopeState
 
 def get_args():
-    parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description='Simulate telescope state from H5 file.')
-    parser.add_option("--ts_db", default=1, type="int", help="Telescope state database number. Default: 1")
-    parser.add_option("--ts_ip", default="127.0.0.1", help="Telescope state ip address. Default: 127.0.0.1")
+    parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description="Simulate telescope state from H5 file.")
+    parser.add_option("--ts-db", default=1, type="int", help="Telescope state database number. Default: 1")
+    parser.add_option("--ts-ip", default="127.0.0.1", help="Telescope state ip address. Default: 127.0.0.1")
     options, args = parser.parse_args()
     if len(args) < 1 or not args[0].endswith(".h5"):
-        parser.error("Please provide a .h5 filename as argument")
+        parser.error("Please provide an h5 filename as an argument.")
     return args[0], options
 
 filename, options = get_args()
 simdata = SimData(filename)
 
-# create TM
+print "Create TS."
 ts = TelescopeState(host=options.ts_ip,db=options.ts_db)
-# use parameters from parameter file to initialise TM
+print "Use parameters from parameter file to initialise TS."
 parameters.init_ts(ts)
-# add and override with TM data from simulator 
+print "Add and override with TS data from simulator."
 simdata.setup_ts(ts)
+print "Done."
+
