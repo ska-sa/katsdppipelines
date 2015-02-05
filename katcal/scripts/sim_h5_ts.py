e49c22d61b77455aceabc97fb1c5d60a673bc83b
Thomas Bennett <thomas@ska.ac.za>
2015-02-05 17:50:48 +0200

    Clean up user interface and make script executable

--- katcal/scripts/sim_h5_ts.py
+++ katcal/scripts/sim_h5_ts.py
@@ -1,28 +1,25 @@
-
-import optparse
-
-from katcal.simulator import SimData
-from katcal import parameters
-
-import sys
-
-from katsdptelstate.telescope_state import TelescopeState
-
+#!/usr/bin/env python
 # ----------------------------------------------------------
 # H5 file to use for simulation
 #   simulation of data and Teselcope Model (TM)
 
-parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description='Simulate telescope state from H5 file.')
-parser.add_option("--ts_db", default=1, type="int", help="Telescope state database number. default: 1")
-parser.add_option("--ts_ip", default="127.0.0.1", help="Telescope state ip address. default: 127.0.0.1")
-(options, args) = parser.parse_args()
+import optparse
 
-if len(args) < 1 or not args[0].endswith(".h5"):
-    print "Please provide an H% filename as argument"
-    sys.exit(1)
-        
-file_name = args[0]
-simdata = SimData(file_name)
+from katcal import parameters
+from katcal.simulator import SimData
+from katsdptelstate.telescope_state import TelescopeState
+
+def get_args():
+    parser = optparse.OptionParser(usage="%prog [options] <filename.h5>", description='Simulate telescope state from H5 file.')
+    parser.add_option("--ts_db", default=1, type="int", help="Telescope state database number. Default: 1")
+    parser.add_option("--ts_ip", default="127.0.0.1", help="Telescope state ip address. Default: 127.0.0.1")
+    options, args = parser.parse_args()
+    if len(args) < 1 or not args[0].endswith(".h5"):
+        parser.error("Please provide a .h5 filename as argument")
+    return args[0], options
+
+filename, options = get_args()
+simdata = SimData(filename)
 
 # create TM
 ts = TelescopeState(host=options.ts_ip,db=options.ts_db)
