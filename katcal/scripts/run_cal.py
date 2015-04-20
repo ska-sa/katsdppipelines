ab0c540e8ddf211355cba61fd7e1f24952e4bd8f
LauraRichter <laura@ska.ac.za>
2015-04-20 14:42:36 +0200

    Integration fiddling

--- katcal/scripts/run_cal.py
+++ katcal/scripts/run_cal.py
@@ -17,10 +17,10 @@ logger.setLevel(logging.DEBUG)
 
 def print_dict(dictionary, ident = '', braces=1):
     """ Recursively prints nested dictionaries."""
-    
+
     for key, value in dictionary.iteritems():
         if isinstance(value, dict):
-           print '%s%s%s%s' %(ident,braces*'[',key,braces*']') 
+           print '%s%s%s%s' %(ident,braces*'[',key,braces*']')
            print_dict(value, ident+'  ', braces+1)
         else:
            print ident+'%s = %s' %(key, value)
@@ -34,13 +34,13 @@ def comma_list(type_):
     return convert
 
 def parse_opts():
-    parser = ArgumentParser(description = 'Set up and wait for a spead stream to run the pipeline.')    
+    parser = ArgumentParser(description = 'Set up and wait for a spead stream to run the pipeline.')
     parser.add_argument('--num-buffers', type=int, default=2, help='Specify the number of data buffers to use. default: 2')
     parser.add_argument('--buffer-maxsize', type=float, default=1000e6, help='The amount of memory (in bytes?) to allocate to each buffer. default: 1e9')
-    # note - the following lines extract various parameters from the MC config    
+    # note - the following lines extract various parameters from the MC config
     parser.add_argument('--cbf-channels', type=int, help='The number of frequency channels in the visibility data. Default from MC config')
     parser.add_argument('--antenna-mask', type=comma_list(str), help='List of antennas in the L0 data stream. Default from MC config')
-    # also need bls ordering    
+    # also need bls ordering
     parser.add_argument('--l0-spectral-spead', type=endpoint.endpoint_list_parser(7200, single_port=True), default=':7200', help='endpoints to listen for L0 SPEAD stream (including multicast IPs). [<ip>[+<count>]][:port]. [default=%(default)s]', metavar='ENDPOINT')
     parser.add_argument('--l1-spectral-spead', type=endpoint.endpoint_parser(7202), default='127.0.0.1:7202', help='destination for spectral L1 output. [default=%(default)s]', metavar='ENDPOINT')
     parser.add_argument('--threading', action='store_true', help='Use threading to control pipeline and accumulator [default: False (to use multiprocessing)]')
@@ -52,7 +52,7 @@ def all_alive(process_list):
     """
     Check if all of the process in process list are alive and return True
     if they are or False if they are not.
-    
+
     Inputs
     ======
     process_list:  list of multpirocessing.Process objects
@@ -62,7 +62,7 @@ def all_alive(process_list):
     for process in process_list:
         alive = alive and process.is_alive()
     return alive
-            
+
 def create_buffer_arrays(buffer_shape,mproc=True):
     """
     Create empty buffer record using specified dimensions
@@ -71,7 +71,7 @@ def create_buffer_arrays(buffer_shape,mproc=True):
         return create_buffer_arrays_multiprocessing(buffer_shape)
     else:
         return create_buffer_arrays_threading(buffer_shape)
-    
+
 def create_buffer_arrays_multiprocessing(buffer_shape):
     """
     Create empty buffer record using specified dimensions,
@@ -85,9 +85,9 @@ def create_buffer_arrays_multiprocessing(buffer_shape):
     data['weights'] = control_method.RawArray(c_float, buffer_size)
     data['times'] = control_method.RawArray(c_double, array_length)
     # assume max 1000 scans!
-    data['track_start_indices'] =  control_method.sharedctypes.RawArray(c_int, 1000) 
+    data['track_start_indices'] =  control_method.sharedctypes.RawArray(c_int, 1000)
     return data
-    
+
 def create_buffer_arrays_threading(buffer_shape):
     """
     Create empty buffer record using specified dimensions,
@@ -101,7 +101,7 @@ def create_buffer_arrays_threading(buffer_shape):
     data['track_start_indices'] = []
     return data
 
-def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=1000e6, 
+def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=1000e6,
            l0_endpoint=':7200', l1_endpoint='127.0.0.1:7202',
            mproc=True):
     """
@@ -109,7 +109,7 @@ def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=100
     This will instantiate num_buffers + 1 threads; a thread for each pipeline and an
     extra accumulator thread the reads data from the spead stream into each buffer
     seen by the pipeline.
-    
+
     Inputs
     ======
     ts: TelescopeState
@@ -126,26 +126,26 @@ def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=100
         Destination endpoint for L1 stream, default: '127.0.0.1:7202'
     mproc: bool
         True for control via multiprocessing, False for control via threading
-    """ 
+    """
 
     # extract data shape parameters from TS
     for k in ts.keys(): print k
-    
+
     print
     print_dict(ts.config)
-    #for k in ts.config: 
+    #for k in ts.config:
     #   print k
     #   for jk in ts.config[k]:
     #      print '   ', jk
     print
-    
+
     print '*', cbf_n_chans, antenna_mask
     nchan = cbf_n_chans
     npol = 4
-    nant = len(antenna_mask.split(','))
+    nant = len(antenna_mask)
     # number of baselines includes autocorrelations
     nbl = nant*(nant+1)/2
-    
+
     # buffer needs to include:
     #   visibilities, shape(time,channel,baseline,pol), type complex64 (8 bytes)
     #   flags, shape(time,channel,baseline,pol), type int8 (? confirm)
@@ -157,24 +157,24 @@ def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=100
     array_length = buffer_maxsize/((scale_factor*nchan*npol*nbl) + time_factor)
     array_length = np.int(np.ceil(array_length))
     logger.info('Max length of buffer array : {0}'.format(array_length,))
-    
+
     # Set up empty buffers
     buffer_shape = [array_length,nchan,npol,nbl]
     buffers = [create_buffer_arrays(buffer_shape,mproc=mproc) for i in range(num_buffers)]
 
     # set up conditions for the buffers
     scan_accumulator_conditions = [control_method.Condition() for i in range(num_buffers)]
-    
+
     # Set up the accumulator
-    accumulator = init_accumulator_control(control_method, control_task, buffers, buffer_shape, scan_accumulator_conditions, l0_endpoint, ts)    
-    
+    accumulator = init_accumulator_control(control_method, control_task, buffers, buffer_shape, scan_accumulator_conditions, l0_endpoint, ts)
+
     #accumulator = accumulator_control(multiprocessing.Process, buffers, buffer_shape, scan_accumulator_conditions, l0_endpoint, ts)
 
     # Set up the pipelines (one per buffer)
     #pipelines = [pipeline_control(buffers[i], buffer_shape, scan_accumulator_conditions[i], i, l1_endpoint, ts) for i in range(num_buffers)]
-    
+
     pipelines = [init_pipeline_control(control_method, control_task, buffers[i], buffer_shape, scan_accumulator_conditions[i], i, l1_endpoint, ts) for i in range(num_buffers)]
-    
+
     # Start the pipeline threads
     map(lambda x: x.start(), pipelines)
     # might need delay here for pipeline threads to aquire conditions then wait?
@@ -195,7 +195,7 @@ def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=100
         print '\nUnknown error\n'
         map(lambda x: x.stop(), pipelines)
         accumulator.stop()
-        
+
     accumulator.join()
     print "Accumulator Stopped"
 
@@ -204,9 +204,9 @@ def run_threads(ts, cbf_n_chans, antenna_mask, num_buffers=2, buffer_maxsize=100
 
 
 if __name__ == '__main__':
-    
+
     opts = parse_opts()
-    
+
     if opts.threading is False:
         import multiprocessing as control_method
         from multiprocessing import Process as control_task
@@ -218,8 +218,8 @@ if __name__ == '__main__':
     # short weit to give me time to start up the simulated spead stream
     # time.sleep(5.)
 
-    run_threads(opts.telstate,  
+    run_threads(opts.telstate,
            cbf_n_chans=opts.cbf_channels, antenna_mask=opts.antenna_mask,
            num_buffers=opts.num_buffers, buffer_maxsize=opts.buffer_maxsize,
-           l0_endpoint=opts.l0_spectral_spead[0], l1_endpoint=opts.l1_spectral_spead, 
+           l0_endpoint=opts.l0_spectral_spead[0], l1_endpoint=opts.l1_spectral_spead,
            mproc=not(opts.threading))
