b40cb360691b0ee771cc1eb6b3f38e84af3e20cc
LauraRichter <laura@ska.ac.za>
2015-05-19 15:58:36 +0200

    Changed spead rate defaults; Changed to run scripts from $PATH

--- katcal/scripts/run_sim.py
+++ katcal/scripts/run_sim.py
@@ -1,4 +1,4 @@
-
+#! /usr/bin/env python
 # ---------------------------------------------------------------------------------------
 # Runs simulator using tmux
 # ---------------------------------------------------------------------------------------
@@ -26,6 +26,7 @@
 import tmuxp
 import time
 from argparse import ArgumentParser
+import os.path
 
 def parse_args():
     parser = ArgumentParser(description = 'Run simulated katcal from h5 file')
@@ -35,8 +36,8 @@ def parse_args():
     parser.add_argument('--h5file', type=str, default='~/data/1427381884.h5', help='H5 file for simulated data')
     parser.add_argument('--buffer-maxsize', type=float, default=1e9, help='The amount of memory (in bytes?) to allocate to each buffer. default: 1e9')
     parser.add_argument('--max-scans', type=int, default=0, help='Number of scans to transmit. Default: all')
-    parser.add_argument('--l0-rate', type=float, default=5e7, help='Simulated L0 SPEAD rate. For laptops, recommend rate of 5e7. Default: 5e7')
-    parser.add_argument('--l1-rate', type=float, default=5e7, help='L1 SPEAD transmission rate. For laptops, recommend rate of 5e7. Default: 5e7')
+    parser.add_argument('--l0-rate', type=float, default=5e7, help='Simulated L0 SPEAD rate. For laptops, recommend rate of 0.2e7. Default: 0.4e7')
+    parser.add_argument('--l1-rate', type=float, default=5e7, help='L1 SPEAD transmission rate. For laptops, recommend rate of 1.2e7. Default: 5e7')
     return parser.parse_args()
 
 def create_pane(sname,tmserver,keep_session=False):
@@ -71,8 +72,6 @@ def create_pane(sname,tmserver,keep_session=False):
 if __name__ == '__main__':
 	opts = parse_args()
 
-	KATCAL_DIR = '$PYTHONPATH'
-
 	# create tmux server
 	tmserver = tmuxp.Server()
 
@@ -83,24 +82,18 @@ if __name__ == '__main__':
 
 	# set up TS in tmux pane
 	sim_ts_pane = create_pane('sim_ts',tmserver,keep_session=opts.keep_sessions)
-	sim_ts_pane.cmd('send-keys','cd {}'.format(KATCAL_DIR,))
-	sim_ts_pane.enter()
-	sim_ts_pane.cmd('send-keys','python scripts/sim_h5_ts.py --telstate {0} --h5file {1}'.format(opts.telstate, opts.h5file))
+	sim_ts_pane.cmd('send-keys','sim_h5_ts.py --telstate {0} --h5file {1}'.format(opts.telstate, opts.h5file))
 	sim_ts_pane.enter()
 
 	# start pipeline running in tmux pane
 	pipeline_pane = create_pane('pipeline',tmserver,keep_session=opts.keep_sessions)
-	pipeline_pane.cmd('send-keys','cd {}'.format(KATCAL_DIR,))
-	pipeline_pane.enter()
-	pipeline_pane.cmd('send-keys','python scripts/run_cal.py --telstate {0} --buffer-maxsize {1} \
+	pipeline_pane.cmd('send-keys','run_cal.py --telstate {0} --buffer-maxsize {1} \
 		--l1-rate {2} --full-l1'.format(opts.telstate, opts.buffer_maxsize, opts.l1_rate))
 	pipeline_pane.enter()
 
 	# start L1 receiver in tmux pane
 	l1_pane = create_pane('l1_receiver',tmserver,keep_session=opts.keep_sessions)
-	l1_pane.cmd('send-keys','cd {}'.format(KATCAL_DIR,))
-	l1_pane.enter()
-	l1_pane.cmd('send-keys','python scripts/sim_l1_receive.py --telstate {0} --h5file {1}'.format(opts.telstate, opts.h5file))
+	l1_pane.cmd('send-keys','sim_l1_receive.py --telstate {0} --h5file {1}'.format(opts.telstate, opts.h5file))
 	l1_pane.enter()
 
 	# wait a couple of seconds to start data flowing
@@ -108,8 +101,6 @@ if __name__ == '__main__':
 
 	# start data flow in tmux pane
 	sim_data_pane = create_pane('sim_data',tmserver,keep_session=opts.keep_sessions)
-	sim_data_pane.cmd('send-keys','cd {}'.format(KATCAL_DIR,))
-	sim_data_pane.enter()
-	sim_data_pane.cmd('send-keys','python scripts/sim_h5_stream.py --telstate {0} --h5file {1} --l0-rate {2} \
+	sim_data_pane.cmd('send-keys','sim_h5_stream.py --telstate {0} --h5file {1} --l0-rate {2} \
           --max-scans {3}'.format(opts.telstate, opts.h5file, opts.l0_rate, opts.max_scans))
 	sim_data_pane.enter()
