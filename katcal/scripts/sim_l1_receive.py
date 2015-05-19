2c3874ed8cb65dee4e7a0427c0e4e8d889ba362c
LauraRichter <laura@ska.ac.za>
2015-05-19 10:59:09 +0200

    Changed the L1 receiver script to write to a copy of the H5 file, not the original file.

--- katcal/scripts/sim_l1_receive.py
+++ katcal/scripts/sim_l1_receive.py
@@ -7,12 +7,14 @@ import spead64_48 as spead
 from katsdptelstate import endpoint, ArgumentParser
 
 import numpy as np
+import shutil
+import os.path
 
 def parse_opts():
     parser = ArgumentParser(description = 'Simulate receiver for L1 data stream')    
     parser.add_argument('--l1-spectral-spead', type=endpoint.endpoint_list_parser(7202, single_port=True), default=':7202', 
             help='endpoints to listen for L1 SPEAD stream (including multicast IPs). [<ip>[+<count>]][:port]. [default=%(default)s]', metavar='ENDPOINT')
-    parser.add_argument('--h5file', type=str, help='H5 file to write data to')
+    parser.add_argument('--h5file', type=str, help='H5 data to write data to')
     parser.set_defaults(telstate='localhost')
     return parser.parse_args()
 
@@ -76,49 +78,53 @@ if __name__ == '__main__':
     l1_data = receive_l1(spead_stream, return_data=return_data)
 
     if opts.h5file:
-        import katdal
-        f = katdal.open(opts.h5file)
+        new_file = '{0}_L1.h5'.format(opts.h5file.split('.')[0],)
+        if os.path.isfile(new_file):
+            print 'L1 file {0} already exists. Not saving L1 data to file.'.format(new_file,)
+        else:
+            shutil.copyfile(opts.h5file,new_file)
 
-        # need info from telescope state
-        ts = opts.telstate
+            import katdal
+            f = katdal.open(new_file)
 
-        data_times, data_vis, data_flags = l1_data
+            data_times, data_vis, data_flags = l1_data
 
-        # number of timestamps in collected data
-        ti_max = len(data_times)
+            # number of timestamps in collected data
+            ti_max = len(data_times)
 
-        vis = np.array(data_vis)
-        times = np.array(data_times)
-        flags = np.array(data_flags)
+            vis = np.array(data_vis)
+            times = np.array(data_times)
+            flags = np.array(data_flags)
 
-        # check for missing timestamps
-        if not np.all(f.timestamps[0:ti_max] == times):
-            if np.all(f.timestamps[0:ti_max-1] == times[0:-1]):
-                print 'SPEAD error: extra final L1 time stamp. Ignoring last time stamp.'
-                ti_max += -1
-            else:
-                raise ValueError('L1 array and h5 array have different timestamps!')
+            # check for missing timestamps
+            if not np.all(f.timestamps[0:ti_max] == times):
+                if np.all(f.timestamps[0:ti_max-1] == times[0:-1]):
+                    print 'SPEAD error: extra final L1 time stamp. Ignoring last time stamp.'
+                    ti_max += -1
+                else:
+                    raise ValueError('L1 array and h5 array have different timestamps!')
 
-        corr_products = f.corr_products
-        cal_bls_ordering = ts.cal_bls_ordering
-        cal_pol_ordering = ts.cal_pol_ordering
+            # get necessary info from telescope state
+            ts = opts.telstate
+            corr_products = f.corr_products
+            cal_bls_ordering = ts.cal_bls_ordering
+            cal_pol_ordering = ts.cal_pol_ordering
+            bchan = ts.cal_bchan
+            echan = ts.cal_echan
 
-        bchan = ts.cal_bchan
-        echan = ts.cal_echan
+            # pack data into h5 correlation product list
+            #    by iterating through h5 correlation product list
+            print 'Writing data to h5 file {0}'.format(new_file)
+            for i, [ant1, ant2] in enumerate(corr_products):
 
-        # pack data into h5 correlation product list
-        #    by iterating through h5 correlation product list
-        print 'Writing data to h5 file {0}'.format(opts.h5file.split('/')[-1])
-        for i, [ant1, ant2] in enumerate(corr_products):
+                # find index of this pair in the cal product array
+                antpair = [ant1[:-1],ant2[:-1]]
+                cal_indx = cal_bls_ordering.index(antpair)
+                # find index of this element in the pol dimension
+                polpair = [ant1[-1],ant2[-1]]
+                pol_indx = cal_pol_ordering.index(polpair)
 
-            # find index of this pair in the cal product array
-            antpair = [ant1[:-1],ant2[:-1]]
-            cal_indx = cal_bls_ordering.index(antpair)
-            # find index of this element in the pol dimension
-            polpair = [ant1[-1],ant2[-1]]
-            pol_indx = cal_pol_ordering.index(polpair)
-
-            # vis shape is (ntimes, nchan, ncorrprod) for real and imag
-            f._vis[0:ti_max,bchan:echan,i,0] = vis[0:ti_max,:,pol_indx,cal_indx].real
-            f._vis[0:ti_max,bchan:echan,i,1] = vis[0:ti_max,:,pol_indx,cal_indx].imag
+                # vis shape is (ntimes, nchan, ncorrprod) for real and imag
+                f._vis[0:ti_max,bchan:echan,i,0] = vis[0:ti_max,:,pol_indx,cal_indx].real
+                f._vis[0:ti_max,bchan:echan,i,1] = vis[0:ti_max,:,pol_indx,cal_indx].imag
 
