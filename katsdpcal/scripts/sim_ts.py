2fe2cbc3be9016d8b54bd8b71cb4b333c5bdcd97
LauraRichter <laura@ska.ac.za>
2015-08-25 16:35:31 +0200

    Fixed more rebase problems

--- katsdpcal/scripts/sim_ts.py
+++ katsdpcal/scripts/sim_ts.py
@@ -3,13 +3,13 @@
 # Simulate the Telescope State from a file
 
 from katsdpcal import pipelineprocs, conf_dir, param_file
-from katcal.simulator import init_simdata
+from katsdpcal.simulator import init_simdata
 from katsdptelstate import ArgumentParser
 import os
 
 def parse_opts():
-    parser = ArgumentParser(description = 'Simulate Telescope State H5 file')    
-    parser.add_argument('--h5file', type=str, help='H5 file for simulated data')
+    parser = ArgumentParser(description = 'Simulate Telescope State from h5 or MS file')    
+    parser.add_argument('--file', type=str, help='H5 or MS file for simulated data')
     parser.add_argument('--parameters', type=str, default=os.path.join(conf_dir,param_file), help='Default pipeline parameter file (will be over written by TelescopeState. [default: {0}]'.format(param_file,))
     parser.set_defaults(telstate='localhost')
     return parser.parse_args()
