a21e99800032621d59ce352d32c77d8d2d944773
LauraRichter <laura@ska.ac.za>
2015-08-25 15:37:56 +0200

    Add module wide config directory and parameter fine info to module __init__.py

--- katsdpcal/katsdpcal/__init__.py
+++ katsdpcal/katsdpcal/__init__.py
@@ -4,3 +4,10 @@ katsdpcal
 
 Calibration pipeline package for MeerKAT.
 """
+
+# Config file location
+from pkg_resources import resource_filename
+conf_dir = resource_filename(__name__, 'conf')
+
+# Default parameter file
+param_file = 'pipeline_parameters_kat7.txt'
