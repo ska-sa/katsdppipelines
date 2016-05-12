72313937b6ca1c0ebc4434a3559cd4e2f12b4337
LauraRichter <laura@ska.ac.za>
2016-05-12 17:50:15 +0200

    Robustify K solver and fix for small solution intervals in solution interval estimator

--- katsdpcal/katsdpcal/calprocs.py
+++ katsdpcal/katsdpcal/calprocs.py
@@ -687,16 +687,8 @@ def k_fit(data,corrprod_lookup,chans=None,refant=0,chan_sample=1,algorithm='adi'
     if not(np.any(chans)): chans = np.arange(data.shape[0])
 
     # -----------------------------------------------------
-    # solve for the bandpass over the channel range
-
-    # stefcal needs the visibilities as a list of [vis,vis.conjugate]
-    vis_and_conj = np.concatenate((data, data.conj()),axis=-1)
-    bpass = stefcal(vis_and_conj, num_ants, corrprod_lookup, weights=1.0, num_iters=1000, ref_ant=refant, init_gain=None, algorithm=algorithm)
-
-    # FT the visibilities to get course estimate of K
-    #   with noisy data, this is more robust than unwrapping the phase
-    ft_vis = np.fft.fft(bpass,axis=0)
-
+    # FT to find visibility space delays
+    ft_vis = np.fft.fft(data,axis=0)
     # get index of FT maximum
     k_arg = np.argmax(np.abs(ft_vis),axis=0)
     if nchans%2 == 0:
@@ -704,19 +696,35 @@ def k_fit(data,corrprod_lookup,chans=None,refant=0,chan_sample=1,algorithm='adi'
     else:
         k_arg[k_arg > ((nchans-1)/2)] -= nchans
 
-    # calculate K from FT sample frequencies
-    coarse_k = 1.*k_arg/(chan_increment*nchans)
+    # calculate vis space K from FT sample frequencies
+    vis_k = 1.*k_arg/(chan_increment*nchans)
 
-    # now remove coarse K estimate then find slope of residual
+    # now determine per-antenna K values
+    coarse_k = np.zeros([num_pol,num_ants])
+    for ai in range(num_ants):
+        k = vis_k[...,(corrprod_lookup[:,0]==ai)&(corrprod_lookup[:,1]==refant)]
+        if k.size > 0: coarse_k[...,ai] = np.squeeze(k)
+    for ai in range(num_ants):
+        k = vis_k[...,(corrprod_lookup[:,1]==ai)&(corrprod_lookup[:,0]==refant)]
+        if k.size > 0: coarse_k[...,ai] = np.squeeze(-1.0*k)
+
+    # apply coarse K values to the data and solve for bandpass
+    v_corrected = np.zeros_like(data)
+    for vi in range(v_corrected.shape[-1]):
+        for p in range(num_pol):
+            for ci, c in enumerate(chans):
+                v_corrected[ci,p,vi] = data[ci,p,vi] * np.exp(-1.0j*2.*np.pi*c*coarse_k[p,corrprod_lookup[vi,0]]) * np.exp(1.0j*2.*np.pi*c*coarse_k[p,corrprod_lookup[vi,1]])
+    # stefcal needs the visibilities as a list of [vis,vis.conjugate]
+    vis_and_conj = np.concatenate((v_corrected, v_corrected.conj()),axis=-1)
+    bpass = stefcal(vis_and_conj, num_ants, corrprod_lookup, weights=1.0, num_iters=1000, ref_ant=refant, init_gain=None, algorithm=algorithm)
+
+    # find slope of the residual bandpass
     delta_k = np.empty_like(coarse_k)
-    # find bandpass phase slopes (delays)
     for i,bp in enumerate(bpass.T):
         # polarisation
         for p in range(num_pol):
-            # apply course K
-            bp_corrected = bp[p] * np.exp(-1.0j*2.*np.pi*coarse_k[p,i]*np.array(chans))
             # np.unwrap falls over in the case of bad RFI - robustify this later
-            bp_phase = np.unwrap(np.angle(bp_corrected),discont=1.9*np.pi)
+            bp_phase = np.unwrap(np.angle(bp[p]),discont=1.9*np.pi)
             A = np.array([ chans, np.ones(len(chans))])
             delta_k[p,i] = np.linalg.lstsq(A.T,bp_phase)[0][0]/(2.*np.pi)
 
@@ -912,7 +920,10 @@ def solint_from_nominal(solint,dump_period,num_times):
         # solution intervals across the total time range
         intervals = num_times/(dumps_per_solint+s)
         # the size of the final fractional solution interval
-        smallest_inc[i] = intervals % int(intervals)
+        if int(intervals) == 0:
+            smallest_inc[i] = 0
+        else:
+            smallest_inc[i] = intervals % int(intervals)
 
     # choose a solint to minimise the final fractional solution interval
     nsolint = solint+solint_check_range[np.where(smallest_inc==max(smallest_inc))[0]]
