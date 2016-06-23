6d2a825097b1576ee9dc5aad988781eb5009d462
LauraRichter <laura@ska.ac.za>
2016-06-23 17:54:35 +0200

    Simulator just uses default option of not transmitting L1 (might go back and change this later)

--- katsdpcal/scripts/sim_l1_receive.py
+++ katsdpcal/scripts/sim_l1_receive.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
         # was our simulator using an H5 or MS file?
         file_class = get_file_format(opts.file)
 
-        if not ts.cal_full_l1:
+        if False:
             print 'Only target L1 stream transmitted. Not saving L1 data to file.'
         else:
             if os.path.isfile(new_file) or os.path.isdir(new_file):
