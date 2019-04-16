d37b4dbe9ae7a724b8f8ab05ad62a4d1f0ba5ea2
KimMcAlpine <kim.mcalpine@gmail.com>
2019-04-16 14:32:22 +0000

    Solve for BCROSS_DIODE
    This commit allows to the pipeline to solve for the
    HV phase as a function of channel using autocorrelation data if the
    noise diode is fired. The solution is formed by :
    * averaging HV with the conjugate of VH
    * taking the mean phase of these averaged visibilities per antenna
    * the solutions on the second polarisation axis are set to zero.
    
    The HV phase is solved for after applying parallel hand calibration solutions
    and the KCROSS_DIODE solutions. These solutions do not commute and must proceed in
    the given order.
    
    The way in which HV phase and delay solutions are applied to the data is also modified.
    Only the solution at the chosen reference antenna is used to correct the data. This is to
    ensure that small differences between the noise-diode calibration and celestial source based
    calibration at a given antenna remain consistent between separate observations with the same
    reference antenna

--- katsdpcal/katsdpcal/scan.py
+++ katsdpcal/katsdpcal/scan.py
@@ -365,6 +365,51 @@ class Scan(object):
         return CalSolutions('G', g_soln, ave_times)
 
     @logsolutiontime
+    def bcross_sol(self, pre_apply=[], nd=None):
+        """
+        Solve for cross hand bandpass phase
+
+        Parameters
+        ----------
+        pre_apply : list of :class:`~.CalSolutions` CalSolutions, optional
+            calibration solutions to apply
+        nd : :class:`np.ndarray` of bool, optional
+            True for antennas with noise diode on, otherwise False,
+            sets solutions with nd False to NaN
+        Returns
+        -------
+        :class:`~.CalSolution`
+            Cross hand phase CalSolution with soltype `BCROSS_DIODE`, shape (pol, nant). The
+            second polarisation has phase set to zero.
+        """
+        corr = self.auto_ant
+
+        modvis = self.pre_apply(pre_apply, corr, cross_pol=True)
+
+        # average over all time
+        av_vis, av_flags, av_weights = calprocs_dask.wavg_full(modvis, corr.tf.cross_pol.flags,
+                                                               corr.tf.cross_pol.weights)
+
+        # Average the HV and complex conjugate of VH together per antenna
+        weighted_data, flagged_weights = calprocs_dask.weight_data(av_vis, av_flags, av_weights)
+        av_weights = da.sum(flagged_weights, axis=-2)
+        av_vis = (weighted_data[:, 0, :] +
+                  np.conjugate(weighted_data[:, 1, :]) /
+                  av_weights)
+
+        # Set phase in the second polarisation axis to zero
+        bcross_phase = da.angle(av_vis)
+        bcross_phase = da.stack([bcross_phase, np.zeros_like(bcross_phase)], axis=1)
+        # Set amplitudes to one
+        bcross_soln = da.exp(1j * bcross_phase)
+        bcross_soln = bcross_soln.compute()
+
+        # Set soln to NaN for antennas where the noise diode didn't fire
+        if nd is not None:
+            bcross_soln = np.where(~nd, np.nan, bcross_soln)
+        return CalSolution('BCROSS_DIODE', bcross_soln, np.average(self.timestamps))
+
+    @logsolutiontime
     def kcross_sol(self, bchan=1, echan=None, chan_ave=1, pre_apply=[], nd=None, auto_ant=False):
         """
         Solve for cross hand delay offset, for full pol data sets (four polarisation products)
@@ -645,13 +690,20 @@ class Scan(object):
                               * channel_freqs[np.newaxis, :, np.newaxis, np.newaxis])
             return self._apply(g_from_k, vis, cross_pol)
         elif soln.soltype in ['KCROSS_DIODE', 'KCROSS']:
-            # average HV delay over all antennas
+            # select HV delay at refant
             channel_freqs = da.asarray(self.channel_freqs)
-            soln = np.nanmedian(soln.values, axis=-1, keepdims=True)
+            soln = soln.values[..., self.refant][..., np.newaxis]
+            soln = np.repeat(soln, self.nant, axis=-1)
+
             g_from_k = da.exp(2j * np.pi * soln[:, np.newaxis, :, :]
                               * channel_freqs[np.newaxis, :, np.newaxis, np.newaxis])
-            g_from_k = da.tile(g_from_k, self.nant)
             return self._apply(g_from_k, vis, cross_pol)
+
+        elif soln.soltype in ['BCROSS_DIODE']:
+            # select HV phase at refant
+            soln = soln.values[..., self.refant][..., np.newaxis]
+            soln = np.repeat(soln, self.nant, axis=-1)
+            return self._apply(soln, vis, cross_pol)
 
         elif soln.soltype is 'B':
             return self._apply(soln_values, vis, cross_pol)
